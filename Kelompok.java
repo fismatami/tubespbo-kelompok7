8e89ecbf663bf5cedd273073626b44f4456ef5dc
fismatami <avilafismatami@gmail.com>
2016-03-18 12:18:47 +0700

    Update Kelompok.java

--- Kelompok.java
+++ Kelompok.java
@@ -1,37 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kelompok7;
 
-/**
- *
- * @author udev
- */
 public class Kelompok {
 	private Mahasiswa[] anggota;
 	private String jurusan;
 	private int noKelp;
-	private int maxAnggota;
+	private final int maxAnggota;
+        private Pembimbing pembimbing;
 	private int nAnggota=0;
         private Kelompok[] k;
-        private Pembimbing pembimbing;
+        
         
         public Kelompok(int noKelp, String jur, Pembimbing pemb){
-            	this.maxAnggota = 5;
-        	this.anggota= new Mahasiswa[maxAnggota];
-        	this.noKelp=noKelp;
-        	this.pembimbing=pemb;
-        	this.jurusan=jur;
+        this.maxAnggota = 5;
+        this.anggota= new Mahasiswa[maxAnggota];
+        this.noKelp=noKelp;
+        this.pembimbing=pemb;
+        this.jurusan=jur;
         }
-        
+
         public Pembimbing getPembimbing() {
-        	return pembimbing;
+        return pembimbing;
         }
 
         public void setPembimbing(Pembimbing pembimbing) {
-        	this.pembimbing = pembimbing;
+        this.pembimbing = pembimbing;
         }
         
 	public void setJurusan(String jurusan) {
@@ -49,20 +41,19 @@ public class Kelompok {
 	public int getNoKelp() {
 		return noKelp;
 	}
-        
+	
 	public void addAnggota(Mahasiswa m) {
 		if (nAnggota<maxAnggota){
-			this.anggota[nAnggota] = m;
-			
-		}else{
+                    this.anggota[nAnggota] = m;
+                }else{
                     System.out.println("Mahasiswa Sudah Penuh");
                 }
                 nAnggota++;
 	}
 	
 	public void removeAnggota(Mahasiswa m){
-               boolean found = false;
-               for(int i=0; i<=nAnggota; i++){
+            boolean found = false;
+               for(int i=0;i<=nAnggota;i++){
                    if(anggota[i].getNim().equals(m.getNim())){
                        found = true;
                        anggota[i]=anggota[nAnggota-1];
@@ -71,15 +62,10 @@ public class Kelompok {
                    }
                }
 	}
-	
-	public void displayKelompok(){
+        public void displayKelompok(){
             //System.out.println("Anggota:\n");
            // for(int i=0; i < maxAnggota; i++){
             System.out.println(" "+(0+1)+". "+anggota[0].getNim());
          //}
-        }
-        
-        
-	
-	
+        }	
 }
